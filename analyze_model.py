154388ff778cf779d90f2ab2ab9e152e2a3b40fc
giovannicampa <giocampa93@gmail.com>
2021-02-21 22:40:39 +0100

    Added plotting of maps

--- analyze_model.py
+++ analyze_model.py
@@ -18,11 +18,11 @@ import cv2
 # Load model from savel_model directory
 save_models_list = os.listdir("saved_model/")
 
-saved_model = save_models_list[0]
+saved_model = save_models_list[1]
 model = tf.keras.models.load_model("saved_model/" + saved_model)
 model.summary()
 
-batch_size_input, width_input, length_input, colors_input = model.input_shape
+batch_size_input, height_input, width_input, colors_input = model.input_shape
 
 
 # gets weights connecting the GAP to the Dense layer
@@ -51,7 +51,7 @@ def generate_cam(image_value, features, results):
     class_activation_weights = gap_weights[:,0]
 
     # upsample to the image size (37 = dimension of the output from the last conv2d)
-    class_activation_features = sp.ndimage.zoom(features_for_img, (width_input/7, length_input/7, 1), order=2)
+    class_activation_features = sp.ndimage.zoom(features_for_img, (height_input/7, width_input/7, 1), order=2)
 
     # compute the intensity of each feature in the CAM
     cam_output  = np.dot(class_activation_features,class_activation_weights)
@@ -90,7 +90,6 @@ def generate_saliency_map(image_value, class_true_idx):
     gradients = tape.gradient(loss, inputs)
 
 
-
     # reduce the RGB image to grayscale
     grayscale_tensor = tf.reduce_sum(tf.abs(gradients), axis=-1)
 
@@ -107,29 +106,6 @@ def generate_saliency_map(image_value, class_true_idx):
     # remove the channel dimension to make the tensor a 2d tensor
     normalized_tensor = tf.squeeze(normalized_tensor)
 
-
-    # # max and min value in the grayscale tensor
-    # print(np.max(grayscale_tensor[0]))
-    # print(np.min(grayscale_tensor[0]))
-    # print()
-
-    # coordinates of the first pixel where the max and min values are located
-    # max_pixel = np.unravel_index(np.argmax(grayscale_tensor[0]), grayscale_tensor[0].shape)
-    # min_pixel = np.unravel_index(np.argmin(grayscale_tensor[0]), grayscale_tensor[0].shape)
-    # print(max_pixel)
-    # print(min_pixel)
-    # print()
-
-    # # these coordinates should have the max (255) and min (0) value in the normalized tensor
-    # print(normalized_tensor[max_pixel])
-    # print(normalized_tensor[min_pixel])
-
-    # plt.figure(figsize=(8, 8))
-    # plt.axis('off')
-    # plt.imshow(normalized_tensor, cmap='gray')
-    # plt.show()
-
-
     gradient_color = cv2.applyColorMap(normalized_tensor.numpy(), cv2.COLORMAP_HOT)
     gradient_color = gradient_color / 255.0
     super_imposed = cv2.addWeighted(image_value[0], 0.5, gradient_color, 0.5, 0.0)
@@ -138,13 +114,16 @@ def generate_saliency_map(image_value, class_true_idx):
 
 
 # utility function to preprocess an image and show the CAM
-def convert_and_classify(image):
+def convert_and_classify(image, class_true_idx):
+    """ Show the interpretabilty of the image
 
+    Plot the class activation and the saliency maps
+    """
     # load the image
     img = cv2.imread(image)
 
     # preprocess the image before feeding it to the model
-    img = cv2.resize(img, (150,150)) / 255.0
+    img = cv2.resize(img, (height_input, width_input)) / 255.0
 
     # add a batch dimension because the model expects it
     tensor_image = np.expand_dims(img, axis=0)
@@ -152,7 +131,7 @@ def convert_and_classify(image):
     # get the features and prediction
     features, results = cam_model.predict(tensor_image)
 
-    # generate the CAM
+    # Generate the interpretability maps
     results, cam_output = generate_cam(tensor_image, features, results)
     super_imposed = generate_saliency_map(tensor_image, class_true_idx = 0)
 
@@ -160,15 +139,23 @@ def convert_and_classify(image):
     print(f'sigmoid output: {results}')
 
     fig, ax = plt.subplots(1,2)
-
+    fig.suptitle(f"Classified as {classes[np.argmax(results[0])]}", fontsize = 17)
     ax[0].imshow(cam_output, cmap='jet', alpha=0.5)
     ax[0].imshow(tf.squeeze(tensor_image), alpha=0.5)
-    ax[0].title.set_text(f"Classified as {classes[np.argmax(results[0])]}")
+    ax[0].set_title("Class activation map", fontsize = 15)
     ax[1].imshow(super_imposed)
+    ax[1].set_title("Saliency map", fontsize = 15)
     plt.show()
 
 
-convert_and_classify('./images/amanita/images359.jpg')
+# Plot a series of pictures for each class
+for i, mushroom in enumerate(classes):
+
+    path_2_picture = "./images/" + mushroom + "/"
+    picture_list = os.listdir(path_2_picture)
+
+    for j in range(10):
+        convert_and_classify(path_2_picture + picture_list[j], class_true_idx = 0)
 
 
 
